17e8f7f2b8482970b8a9706a021c8772cba8235c
Kingsley <dondecency11@gmail.com>
2022-10-10 03:29:30 +0000

    giving changes

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,6 @@
-#!/usr/bin/python3
-for i in range(90):
-    if (i % 10) > (i / 10):
-        if i < 89:
-            print("{:02d},".format(i), end="")
+for i in range(9):
+    for j in range(i + 1, 10):
+        if i == 8:
+            print("{}{}".format(i, j))
         else:
-            print("{:02d}".format(i))
+            print("{}{}".format(i, j), end=", ")
