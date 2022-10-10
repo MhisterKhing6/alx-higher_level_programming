0494f830479d9f688c31f595e095132abce614ca
Kingsley <dondecency11@gmail.com>
2022-10-10 03:38:26 +0000

    Last Message

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for i in range(100):
-    if i <= 98:
-        print("{0:02},".format(i), end="")
+    if i == 99:
+        print(i)
     else:
-        print("{0:02d}".format(i), end="")
+        print("{:0>2d}".format(i), end=", ")
