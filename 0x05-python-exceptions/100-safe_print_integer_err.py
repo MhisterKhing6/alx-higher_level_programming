ab2f573a61cee3061d136973dc88b40f54d74d5e
Kingsley <dondecency11@gmail.com>
2022-10-11 12:52:26 +0000

    how"

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 import sys
+
+
 def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
@@ -9,7 +11,3 @@ def safe_print_integer_err(value):
     except ValueError as v:
         print(v, file=sys.stderr)
         return False
-value = "School"
-has_been_print = safe_print_integer_err(value)
-if not has_been_print:
-    print("{} is not an integer".format(value))
