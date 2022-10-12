f2ad39aa25177b4c81d488338817e530344a0bb7
Kingsley <dondecency11@gmail.com>
2022-10-12 10:18:45 +0000

    5 th Task

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -6,16 +6,7 @@ class Square:
     """ define __init__ function """
     def __init__(self, size=0):
         """ initializes size of self with size """
-        """ if statement """
-        if type(value) is not int:
-            """ raise error """
-            raise TypeError("size must be an integer")
-        elif value < 0:
-            """ raise error """
-            raise ValueError("size must be >= 0")
-        else:
-            """ initialize """
-            self.__size = value
+        self.size = size
 
     @property
     def size(self):
@@ -38,3 +29,12 @@ class Square:
     def area(self):
         """ returns area """
         return self.__size ** 2
+    """ defines my_print function """
+    def my_print(self):
+        if self.__size == 0:
+            print()
+        else:
+            for i in range(self.__size):
+                for j in range(self.__size):
+                    print("#", end="")
+                print()
