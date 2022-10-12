a14abbd6e978b18a34594cd012fc2edfde08e055
Kingsley <dondecency11@gmail.com>
2022-10-12 09:31:07 +0000

    Second Push

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Creates a square class and defines it using its base"""
+
+
+class Square:
+    """A square class with bass as only form of definition"""
+
+    def __init__(self, size=0):
+        """initialize to size variable"""
+        if size is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must b e >= 0")
+        else:
+            self.__size = size
