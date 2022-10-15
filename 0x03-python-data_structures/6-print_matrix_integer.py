2b63a70081e0a0d42cc842ee4358eed1e0426488
Kingsley <dondecency11@gmail.com>
2022-10-15 05:20:14 +0000

    6 th push

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    """Get rows in the mat, get values from rows """
+    for row in matrix:
+        for values in row:
+            print("{:d} ".format(values), end=" ")
+        print()
