a3e6aa07b4540a33278933626a9597a1921ae766
Kingsley <dondecency11@gmail.com>
2022-10-15 05:39:45 +0000

    6 th project

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    """Get rows in the mat, get values from rows """
     for row in matrix:
-        for values in row:
-            print("{:d}".format(values), end=" ")
+        for col in row:
+            print("{:d}".format(col), end=" " if col != row[-1] else "")
         print()
