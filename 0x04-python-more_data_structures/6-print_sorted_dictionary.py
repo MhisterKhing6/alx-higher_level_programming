8213dba2c995927329ccba33b6b0a6cf7d4f08d9
Kingsley <dondecency11@gmail.com>
2022-10-16 06:57:11 +0000

    9 th push

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -4,3 +4,7 @@ def print_sorted_dictionary(a_dictionary):
     ke.sort()
     for key in ke:
         print("{:s}: {}".format(key, a_dictionary[key]))
+
+a_dictionary = { 'language': "C", 'Number': 89, 'track': "Low level", 'ids': [1, 2, 3] }
+print_sorted_dictionary(a_dictionary)
+
