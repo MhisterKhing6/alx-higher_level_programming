6a19ea09cc77ec2438c67e93042ed48781663bd9
Kingsley <dondecency11@gmail.com>
2022-10-24 11:04:38 +0000

    First advance push

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,7 +1,16 @@
 #!/usr/bin/python3
 class MyInt(int):
+    """MyInt: An integer class"""
     def __eq__(self, other):
+        """__eq__:for equal to comparison
+         :arg
+         :param other: an iteger to compare to other
+         :return true if integer != other, else true"""
         return super(MyInt, self).__ne__(other)
 
     def __ne__(self, other):
+        """__ne__:for not to comparison
+                 :arg
+                 :param other: an iteger to compare to other
+                 :return true if integer == other, else true"""
         return super(MyInt, self).__eq__(other)
