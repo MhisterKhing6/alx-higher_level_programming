913d916cd2399304d319ce0de822d0c8fbad225d
Kingsley <dondecency11@gmail.com>
2022-10-24 10:00:22 +0000

    First push

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
 """ returns the attribute and methods of an object"""
 
+"""
+lookup - returns the list of available attributes and methods of an object
+@obj: object
+Return: list object
+"""
+
 
 def lookup(obj):
     return dir(obj)
