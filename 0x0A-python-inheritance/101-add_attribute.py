c470238457e480c0404dc2717728c34e66189535
Kingsley <dondecency11@gmail.com>
2022-10-24 11:10:09 +0000

    Last advance push

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
-
+"""Check to see if an attribute can be added to a class"""
 def add_attribute(a, b, c):
+    """add_attribute: Add an attribute b of value c to class
+    :arg
+    :param a:An object for attribute to be added
+    :param b: name of attribute
+    :param c:  value of attribute
+    :return None upon success else exception"""
     if b in dir(a) or type(a) is str:
         raise TypeError("can't add new attribute")
     a.__dict__[b] = c
