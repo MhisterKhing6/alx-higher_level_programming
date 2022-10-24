5626a6d4c42a446861cfd28cfba3f03970ee9494
Kingsley <dondecency11@gmail.com>
2022-10-24 10:02:35 +0000

    Fisrt push

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-""" returns the attribute and methods of an object"""
-
 """
 lookup - returns the list of available attributes and methods of an object
 @obj: object
@@ -9,4 +7,5 @@ Return: list object
 
 
 def lookup(obj):
+    """ return list of obj """
     return dir(obj)
