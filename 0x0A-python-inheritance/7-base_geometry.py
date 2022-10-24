d787a7caf970c39136b5205a07cf8c7b1b462326
Kingsley <dondecency11@gmail.com>
2022-10-24 10:29:29 +0000

    8 th push

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -3,11 +3,19 @@
 
 
 class BaseGeometry:
+    """BaseGeometry: Creates an empty baseGeo class"""
     def area(self):
         """Area with exception"""
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
+        """
+        integer_validator: Validate an integer
+        :arg
+        :param name: name of the integer
+        :param value: value
+        :return: None upon successful validation else raise exception
+        """
         if type(value) != int:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
