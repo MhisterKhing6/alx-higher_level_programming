1faf0372e18baa2d881c1bba25d4eacb1170f6cd
Kingsley <dondecency11@gmail.com>
2022-10-24 10:33:57 +0000

    8 push

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -15,7 +15,15 @@ class BaseGeometry:
 
 
 class Rectangle(BaseGeometry):
+    """Rectangle: Create a rectangle class
+    """
     def __init__(self, width, height):
+        """__init__: Initialize the and validate the rectangle class
+        :arg
+        :param width: with of the rectangle must be integer and greater than zero
+        :param height: height of the rectangle must be integer and greater than zero
+        :return: void
+        """
         super(Rectangle, self).__init__()
         super().integer_validator("width", width)
         super().integer_validator("height", height)
