882aac2cc66f2e407f76299f6a9e880ab3be8d3a
Kingsley <dondecency11@gmail.com>
2022-10-24 10:36:23 +0000

    9 th push

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,13 +1,22 @@
 #!/usr/bin/python3
-"""Creates an empty baseGeo class"""
+"""Creates an rectangle and geom class"""
+
 
 
 class BaseGeometry:
+    """BaseGeometry: Creates an empty baseGeo class"""
     def area(self):
         """Area with exception"""
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
+        """
+        integer_validator: Validate an integer
+        :arg
+        :param name: name of the integer
+        :param value: value
+        :return: None upon successful validation else raise exception
+        """
         if type(value) != int:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
