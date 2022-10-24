88ad4df8f940bb19b6e5d4db5b019f9341fe9dc4
Kingsley <dondecency11@gmail.com>
2022-10-24 10:16:39 +0000

    Second push

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -3,4 +3,12 @@
 
 
 def is_kind_of_class(obj, a_class):
+    """
+        is_kind_of_class - check if an object is a subb class of an instance of a given class.
+        Args:
+            obj: The object to check.
+            a_class: The class to match the type of obj to.
+        Returns: If obj is exactly an instance of a_class - True,
+                Otherwise - False.
+        """
     return issubclass(type(obj), a_class)
