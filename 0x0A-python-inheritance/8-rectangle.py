acc74ccaf319b4a0382ae41fcb9920486b4d8f50
Kingsley <dondecency11@gmail.com>
2022-10-24 10:38:12 +0000

    9 the push

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,40 +1,23 @@
 #!/usr/bin/python3
-"""Creates an rectangle and geom class"""
-
-
-
-class BaseGeometry:
-    """BaseGeometry: Creates an empty baseGeo class"""
-    def area(self):
-        """Area with exception"""
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """
-        integer_validator: Validate an integer
-        :arg
-        :param name: name of the integer
-        :param value: value
-        :return: None upon successful validation else raise exception
-        """
-        if type(value) != int:
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be  greater than 0".format(name))
+"""
+8-rectangle module
+"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
-    """Rectangle: Create a rectangle class
+    """
+    Represents a rectangle.
+    Inherits from BaseGeometry.
     """
     def __init__(self, width, height):
-        """__init__: Initialize the and validate the rectangle class
-        :arg
-        :param width: with of the rectangle must be integer and greater than zero
-        :param height: height of the rectangle must be integer and greater than zero
-        :return: void
         """
-        super(Rectangle, self).__init__()
-        super().integer_validator("width", width)
-        super().integer_validator("height", height)
+        Initializes an instance.
+        Args:
+            width: width of the rectangle
+            height: height of the rectangle
+        """
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
         self.__width = width
         self.__height = height
