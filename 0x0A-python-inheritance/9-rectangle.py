c73395f3b1036b14eeb756ac3c01a2206f2729aa
Kingsley <dondecency11@gmail.com>
2022-10-24 10:41:43 +0000

    10th push

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -1,29 +1,36 @@
+
 #!/usr/bin/python3
-"""Creates an empty baseGeo class"""
-
-
-class BaseGeometry:
-    def area(self):
-        """Area with exception"""
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        if type(value) != int:
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be  greater than 0".format(name))
+"""
+8-rectangle module
+"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
+    """
+    Represents a rectangle.
+    Inherits from BaseGeometry.
+    """
     def __init__(self, width, height):
-        super(Rectangle, self).__init__()
-        super().integer_validator("width", width)
-        super().integer_validator("height", height)
+        """
+        Initializes an instance.
+        Args:
+            width: width of the rectangle
+            height: height of the rectangle
+        """
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
         self.__width = width
         self.__height = height
 
     def area(self):
+        """area: Calculate the area
+        :return: lenght * width"""
         return self.__width * self.__height
 
     def __str__(self):
+        """
+        __str__:Strings representation of the rectangle class
+        :return:String representing the class
+        """
         return "[Rectangle] {}/{}".format(self.__width, self.__height)
