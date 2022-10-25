7b06a8971be26e6b13ff6456949ba563772bebdc
Kingsley <dondecency11@gmail.com>
2022-10-25 10:30:02 +0000

    4th push

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
+"""
+3-to_json_string module
+"""
 import json
-"""convert to json string implementation"""
 
 
 def to_json_string(my_obj):
-    """to_json_string : convert object to json object
-    :arg
-    :param my_obj : object to convert to json implementation
-    :return: jason serilization of the object"""
+    """
+    to_json_string - returns the JSON representation of an object (string):
+    Args:
+        my_obj: string to represent
+    Return: json representation
+    """
     return json.dumps(my_obj)
