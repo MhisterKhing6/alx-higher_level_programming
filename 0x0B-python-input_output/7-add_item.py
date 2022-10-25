a281066ff836b8b82773238f9f5882e415e03f5b
Kingsley <dondecency11@gmail.com>
2022-10-25 11:28:28 +0000

    Jason file

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,9 +1,20 @@
 #!/usr/bin/python3
+"""
+7-add_item module
+"""
 import sys
-save_to_json = __import__('5-save_to_json_file').save_to_json_file
+import json
+import os.path
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-"""Add all command line argument to a json file and save them"""
-save_to_json(sys.argv[1:], "add_item.json")
-load_from_json_file("add_item.json")
+file = "add_item.json"
+json_list = []
 
+if os.path.exists(file):
+    json_list = load_from_json_file(file)
+
+for i in range(1, len(sys.argv)):
+    json_list.append(sys.argv[i])
+
+save_to_json_file(json_list, file)
