d1eed496c289c10f52ed04d115c7794aa980a992
Kingsley <dondecency11@gmail.com>
2022-12-23 05:31:12 +0000

    how

---
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+const dict = require('./101-data').dict;
+const newdict = {};
+for (const value of Object.values(dict)) {
+  const ls = [];
+  for (const key of Object.keys(dict)) {
+    if (value === dict[key]) {
+      ls.push(key);
+    }
+  }
+  newdict[value] = ls;
+}
+console.log(dict);
+console.log(newdict);
