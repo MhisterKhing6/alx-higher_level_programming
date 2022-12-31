07590cb4bd332b44d2768c3e3847f0b38755ace7
Kingsley <dondecency11@gmail.com>
2022-12-31 06:25:28 +0000

    1

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+"""
+    Python MySQLdb introduction
+    Select all states from the states database
+"""
+if __name__ == '__main__':
+    import sys
+    import MySQLdb
+
+    # create a connection to database
+    connect = MySQLdb.connect(host='localhost', user=sys.argv[1],
+                              passwd=sys.argv[2], db=sys.argv[3], port=3036)
+    #  get a cursor
+    cursor = connect.cursor()
+    cursor.execute('''
+                   SELECT * from states
+                   WHERE states.name like('N%')
+                   ''')
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
