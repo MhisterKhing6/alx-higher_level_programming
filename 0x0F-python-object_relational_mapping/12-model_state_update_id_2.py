36404216225de44a82d428991fbaca5c46b63c7c
Kingsley <dondecency11@gmail.com>
2023-01-02 06:13:33 +0000

    12

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Add Louisiana values to the states table
+"""
+import sys
+from sqlalchemy.orm import sessionmaker
+
+from model_state import State
+
+from sqlalchemy import (create_engine)
+
+if __name__ == "__main__":
+    # get a session
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost:3306/{}'.
+        format(sys.argv[1], sys.argv[2], sys.argv[3]),
+        pool_pre_ping=True
+    )
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    upd = session.query(State).filter(State.id == 2).first()
+    upd.name = "New Mexico"
+    session.add(upd)
+    session.commit()
+
