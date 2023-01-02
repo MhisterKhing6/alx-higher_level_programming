9c0642757b1651680cc796d508e59f7d1d53ce38
Kingsley <dondecency11@gmail.com>
2023-01-02 05:38:36 +0000

    10

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+List all states that contains an A in the database
+"""
+import sys
+from sqlalchemy.orm import sessionmaker
+
+from model_state import State
+
+from sqlalchemy import (create_engine)
+
+if __name__ == "__main__":
+    # get a session
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost:3306/{}'.
+        format(sys.argv[1], sys.argv[2], sys.argv[3]),
+        pool_pre_ping=True
+    )
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    result = session.query(State).filter(State.name == sys.argv[4])\
+        .order_by(State.id).first()
+    if result:
+        print(result.id)
+    else:
+        print("Not found")
