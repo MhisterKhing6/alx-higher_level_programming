e762dda62c806f499aaced1bd48aa681a11e83e8
Kingsley <dondecency11@gmail.com>
2023-01-16 07:22:14 +0000

    2

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 # Create get the size of a page
-if(("$#" != 0))
-then 
-  t_size=$(curl -w '%{size_download}' -s -o /dev/null  "$1")
-  h_size=$(curl -w '%{size_header}' -s -o /dev/null "$1")
-  echo "$((t_size - h_size))"
-fi
+curl -w '%{size_download}' -s -o /dev/null  "$1"
+
