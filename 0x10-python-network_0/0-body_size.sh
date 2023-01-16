699677994d89e3bb0dbc448bffa9f5494f95e19f
Kingsley <dondecency11@gmail.com>
2023-01-16 07:19:11 +0000

    push

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 # Create get the size of a page
 if(("$#" != 0))
-then curl -w '%{size_download}' -s -o _ "$1"
+then 
+  t_size=$(curl -w '%{size_download}' -s -o /dev/null  "$1")
+  h_size=$(curl -w '%{size_header}' -s -o /dev/null "$1")
+  echo "$((t_size - h_size))"
 fi
