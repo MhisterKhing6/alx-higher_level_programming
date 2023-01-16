60d668e8a64b9fdfc91547a674d65155a2e564f7
Kingsley <dondecency11@gmail.com>
2023-01-16 12:30:41 +0000

    how"

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #Create get the size of a page
-curl -G -b "X-School-User-Id=89" -s  "$1"
+curl -G -H "X-School-User-Id: 89" -s  "$1"
---
+++ 0x10-python-network_0/5-post_params.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+#Create get the size of a page
+curl -F email=test@gmail.com -F subject='I will always be here for PLD' -s  "$1"
