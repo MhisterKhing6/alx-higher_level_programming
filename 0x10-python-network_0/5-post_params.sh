b5049c41489aaa3384cab44c5aa545cfc2ba8aa0
Kingsley <dondecency11@gmail.com>
2023-01-16 12:39:45 +0000

    how

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #Create get the size of a page
-curl -F email=test@gmail.com -F subject='I will always be here for PLD' -s  "$1"
+curl -X POST -d email=test@gmail.com -d subject='I will always be here for PLD' -s  "$1"
