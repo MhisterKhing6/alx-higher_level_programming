67a6db11312fd65fe35ad0735ebe80540e0c8ac4
Kingsley <dondecency11@gmail.com>
2023-01-17 03:37:30 +0000

    how

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" Finds a peak in a list of unsorted integers
+"""
+
+
+def find_peak(list_of_integers):
+    list_l = len(list_of_integers)
+    if list_l is 0:
+        return None
+    peak = binary_search(list_of_integers, 0, list_l - 1)
+    return list_of_integers[peak]
+
+
+def binary_search(a, lo, hi):
+    if lo >= hi:
+        return lo
+    mid = ((hi - lo) // 2) + lo
+    if a[mid] > a[mid + 1]:
+        return binary_search(a, lo, mid)
+    else:
+        return binary_search(a, mid + 1, hi)
