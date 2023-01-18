e2007ebee2db2271f204b2169c4ba5c3782c3faf
Kingsley <dondecency11@gmail.com>
2023-01-18 12:33:19 +0000

    4

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""  fetches https://intranet.hbtn.io/status  """
+import requests
+
+
+if __name__ == "__main__":
+    body = requests.get('https://intranet.hbtn.io/status').content
+    print("Body response:")
+    print("\t- type: {}".format(type(body)))
+    print("\t- content: {}".format(body))
+    print("\t- utf8 content: {}".format(body.decode("utf-8")))
