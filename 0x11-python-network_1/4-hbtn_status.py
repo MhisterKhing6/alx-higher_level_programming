e286745eb54f14d221e9d79232da24fc8422babf
Kingsley <dondecency11@gmail.com>
2023-01-18 12:40:21 +0000

    4

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -3,7 +3,7 @@
 import requests
 
 if __name__ == "__main__":
-    body = requests.get('https://intranet.hbtn.io/status').text
+    body = requests.get('https://intranet.hbtn.io/status')
     print("Body response:")
-    print("\t- type: {}".format(type(body)))
-    print("\t- content: {}".format(body))
+    print("\t- type: {}".format(type(body.text)))
+    print("\t- content: {}".format(body.text))
