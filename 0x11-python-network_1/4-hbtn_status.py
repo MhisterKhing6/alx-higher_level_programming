748a0d9503f6613ff5ca305b0537b2c551038ee7
Kingsley <dondecency11@gmail.com>
2023-01-18 13:02:11 +0000

    how

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
-"""  fetches https://intranet.hbtn.io/status  """
+"""
+Python script that fetches https://intranet.hbtn.io/status
+"""
 import requests
 
+
 if __name__ == "__main__":
-    body = requests.get('https://intranet.hbtn.io/status')
+    res = requests.get("https://intranet.hbtn.io/status")
     print("Body response:")
-    print("\t- type: {}".format(type(body.text)))
-    print("\t- content: {}".format(body.text))
+    print("\t- type: {}".format((type(res.text))))
+    print("\t- content: {}".format(res.text))
