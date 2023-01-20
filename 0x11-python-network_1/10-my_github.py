cbbb56526be3d5756f61fec905686f0b4ee067d8
Kingsley <dondecency11@gmail.com>
2023-01-20 02:49:19 +0000

    10

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Display the user id from github"""
+import requests
+from requests.auth import HTTPBasicAuth
+import sys
+if __name__ == "__main__":
+    r = requests.get('https://api.github.com/user',
+                     auth=HTTPBasicAuth(sys.argv[1],
+                                        sys.argv[2]))
+    if r.status_code < 400:
+        try:
+            db = r.json()
+            print(db.get('id'))
+        except ValueError as err:
+            pass
